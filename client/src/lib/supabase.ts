f0ad20173e7f9a17ffa7a3031c83b568ddd0d72c
Gday <gday@coach-os.com>
2026-02-28 02:44:14 +0800

    config: Add Supabase credentials and env file

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://uztndpibiwgzcovrfrtk.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dG5kcGliaXdnemNvdnJmcnRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMzkyNTgsImV4cCI6MjA4NzYxNTI1OH0.n8MNkVsJ3CwvTvJJCpuobWtivxKKv8WbbvmkAsoX3-4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
